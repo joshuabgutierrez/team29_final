9e58d5f9ce30eec3ce8a2dbfdee2b2a391e2e67b
Tristan <ricemaking69@gmail.com>
2024-04-29 13:04:17 -0500

    Recipes page created.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Homepage';
+import Recipes from './Recipes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/recipes" element={<Recipes />} />
       </Routes>
     </BrowserRouter>
   );
