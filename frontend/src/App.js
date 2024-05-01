3b64181f4951c7083247818c8a2288a5e1c7dc69
Tristan <ricemaking69@gmail.com>
2024-05-01 16:01:01 -0500

    Update and more secure handling with the username and email

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,39 @@
+import React, { useState, useEffect  } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Homepage';
 import Recipes from './Recipes';
 import Register from './Register';
 import Account from './Account';
+import UpdateAccount from './UpdateAccount';
 import Login from './Login';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    if (storedEmail) {
+      setEmail(storedEmail);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/" element={<Login setUsername={setUsername}/>} />
+        <Route path="/homepage" element={<Homepage  />} />
         <Route path="/recipes" element={<Recipes />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/account" element={<Account username={username} email={email} />} />
+        <Route path="/updateaccount" element={<UpdateAccount />} />
+        <Route path="/login" element={<Login setUsername={setUsername}/>} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
