cfd46df1a315c35bb2f3a514836603c40bcbfdcb
Joshua Gutierrez <joshgm15@gmail.com>
2024-05-02 19:55:23 -0500

    Added controllers for retreiving a recipe and recipes created by a user

--- backend/api/routes/recipeRouter.js
+++ backend/api/routes/recipeRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { getAllRecipes, createRecipe, updateRecipe, deleteRecipe, likeRecipe, unlikeRecipe } = require("../controllers/recipeController");
+const { getAllRecipes, createRecipe, updateRecipe, deleteRecipe, likeRecipe, unlikeRecipe, getMyRecipes, getSingleRecipe } = require("../controllers/recipeController");
 const { protect, authorize } = require("../../middleware/authMiddleware");
 const { authorizeRecipe } = require("../../middleware/recipeMiddleware");
 
 const router = express.Router();
 
+router.get("/:recipeId", protect, getSingleRecipe);
 router.get("/all", protect, getAllRecipes);
+router.get("/myall", protect, getMyRecipes);
 router.post("/create", createRecipe);
 router.put("/update/:recipeId", protect, authorizeRecipe, updateRecipe);
 router.delete("/delete/:recipeId", protect, authorizeRecipe, deleteRecipe);
