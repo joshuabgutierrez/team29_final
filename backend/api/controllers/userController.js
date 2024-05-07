a09a8a48e19001243901d74eeab0ae91169b1b67
Joshua Gutierrez <joshgm15@gmail.com>
2024-05-07 18:22:48 -0500

    user retrieval by id

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -255,3 +255,27 @@ exports.unfollowUser = async (req, res) => {
         });
     }
 };
+
+exports.getUserById = async (req, res) => {
+    const {userId} = req.params;
+
+    try {
+        const user = await User.findById(userId);    
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        res.status(200).json({
+            message: "User retrieved successfully",
+            user,
+        });
+    } catch (error) {
+        console.error("Error retrieving a user: ", error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+};
--- backend/api/routes/userRouter.js
+++ backend/api/routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { registerUser, getAllUsers, loginUser, updateUser, deleteUser, followUser, unfollowUser } = require("../controllers/userController");
+const { registerUser, getAllUsers, loginUser, updateUser, deleteUser, followUser, unfollowUser, getUserById } = require("../controllers/userController");
 const { protect, authorize } = require("../../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.get("/:userId", protect, getUserById);
 router.get("/all", getAllUsers);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
